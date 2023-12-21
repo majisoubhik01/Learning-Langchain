363421079efc89fd54dd2bce40e580e622c357e0
Soubhik Maji <maji.soubhik2001@gmail.com>
2023-12-21 19:18:17 +0530

    Update main.py

--- Pycode/prod/facts/main.py
+++ Pycode/prod/facts/main.py
@@ -1,8 +1,12 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceHubEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
+
+embeddings = HuggingFaceHubEmbeddings()
 
 text_splitter = CharacterTextSplitter(
     separator="\n",
@@ -15,6 +19,23 @@ docs = loader.load_and_split(
     text_splitter=text_splitter
 )
 
-for doc in docs:
-    print(doc.page_content)
+db = Chroma.from_documents(
+    docs,
+    embedding = embeddings,
+    persist_directory="emb"
+)
+
+results = db.similarity_search_with_score(
+    "Tell me an interesting fact about English language."
+)
+
+# print(results)
+for result in results:
     print("\n")
+    print(result[1])
+    print(result[0].page_content)
+
+    
+# for doc in docs:
+#     print(doc.page_content)
+#     print("\n")
